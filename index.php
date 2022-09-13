964f65cb3bbdf0c74b45241bd853d4fc3de38ec3
victorgsa1 <vgsa@hotmail.com>
2022-09-12 22:17:01 -0300

    AH TA EAZy

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title> .:: Login ::. </title>
+<title> .:: AH TA EAZY ::. </title>
 
 <link rel="stylesheet" type="text/css" href="css/formatacao.css">
 <style type="text/css">

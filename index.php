925f3e362f54e7d89e22c14bf046c217edcdd82c
victorgsa1 <vgsa@hotmail.com>
2022-09-12 22:21:49 -0300

    ah ta easy

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title> .:: AH TA EAZY ::. </title>
+<title> .:: AH TA EAZYa ::. </title>
 
 <link rel="stylesheet" type="text/css" href="css/formatacao.css">
 <style type="text/css">
